aca715d9d62dab6a18b15fb4bea0874463e657ca
Abhishek Anand <anandabhishek3607@gmail.com>
2023-08-22 00:36:22 +0530

    more into sessions

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const app = express();
 
 const session = require('express-session')
 
-app.use(session({ secret: 'thisisnotagood secret' }));
+const sessionOptions = { secret: 'thisisnotagood secret', resave: false, saveUninitialized: false }
 
-app.get('/viewcount', (re, res) => {
+app.use(session(sessionOptions));
+
+app.get('/viewcount', (req, res) => {
     if (req.session.count) {
         req.session.count += 1;
     } else {
@@ -15,6 +17,17 @@ app.get('/viewcount', (re, res) => {
     res.send(`You have viewed this page ${req.session.count} times`)
 })
 
+
+app.get('/register', (req, res) => {
+    const { username = "anonymous" } = req.query;
+    req.session.username = username;
+    res.redirect('/greet')
+})
+
+app.get('/greet', (req, res) => {
+    const { username } = req.session;
+    res.send(`Welcome back, ${username}`)
+})
 app.listen(3000, () => {
     console.log('servig on port 3000')
 })
